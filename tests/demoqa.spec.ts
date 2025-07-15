0506bcb8dce39b8772d104447cd74de4261e28a9
claudia.placinta <claudia.placinta@eggs.ro>
2025-07-15 16:45:13 +0300

    filln phone number

--- tests/demoqa.spec.ts
+++ tests/demoqa.spec.ts
@@ -1,20 +1,36 @@
-import { test, expect } from '@playwright/test';
+import {expect, test} from '@playwright/test';
 
 let page;
 
-test.beforeEach(async ({ browser }) => {    page = await browser.newPage();
+test.beforeEach(async ({browser}) => {
+    page = await browser.newPage();
     await page.goto('https://demoqa.com/automation-practice-form');
-    await expect(page.locator('.app_logo', { hasText: 'Practice Form' })).toBeVisible();
+    await expect(page.locator('.text-center', {hasText: 'Practice Form'})).toBeVisible();
     await page.waitForTimeout(1000);
+});
 
-test('Fill first name, last name and email', { tag: ['@withHooks'] },async ({  }) => {
-    await page.getByPlaceholder('First Name').fill('Ana');
-    await page.getByPlaceholder('Last Name').fill('Banana');
-    await page.getByPlaceholder('name@example.com').fill('anabanana@gmail.com');
+    test('Fill first name, last name and email', {tag: ['@withHooks']}, async ({}) => {
+        await page.getByPlaceholder('First Name').fill('Ana');
+        await page.getByPlaceholder('Last Name').fill('Banana');
+        await page.getByPlaceholder('name@example.com').fill('anabanana@gmail.com');
     });
 
-test('Select the gender', { tag: ['@withHooks'] },async ({  }) => {
+    test('Select the gender', {tag: ['@withHooks']}, async ({}) => {
         await page.locator('//label[text()="Female"]').click();    // Expect a title "to contain" a substring.
     });
 
+test('Fill mobile number and check the number', {tag: ['@withHooks']}, async ({}) => {
+    await page.getByPlaceholder('Mobile Number').fill('0756345275');
+    const phoneInput = await page.getByPlaceholder('Mobile Number'); // înlocuiește cu selectorul real
+    await expect(phoneInput).toBeVisible();
+    const phoneValue = await phoneInput.inputValue();
+
+    // Verifică dacă este un număr format exact din 10 cifre
+    const isValidPhone = /^\d{10}$/.test(phoneValue);
+
+    // Afișează rezultatul
+    console.log(`Numărul de telefon este ${isValidPhone ? 'valid' : 'invalid'}`);
+
+    // Așteptare logică pentru test
+    expect(isValidPhone).toBe(true);
 });
